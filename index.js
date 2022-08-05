a6cd7c58260c4cedaec6b12a9f6b43b892bec422
Michael Polyakov <mishavp2001@yahoo.com>
2022-08-05 12:06:05 -0700

    feat: test change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    resp.send({'hi': 'there'});
+    resp.send({'hi': 'hello'});
 } )
 
 const PORT = process.env.PORT || 5001;
